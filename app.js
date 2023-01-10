3cc0f809029452c0b86cb9eea6e23b64db8585c6
AaronMorimoto <119596908+AaronMorimoto@users.noreply.github.com>
2023-01-10 13:12:03 +0000

    [java test3]

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 alert('Hello World');
+
+let hello = 'Hello World';
+alert (hello);
+
+let int1 = 1;
+alert (int1);
+let int2 = 10;
+alert (int2);
+let float1 = 3.14;
+alert (float1);
+let str1 = 'JavaScriptを覚えよう';
+alert (str1);
+
+alert (3+4);
