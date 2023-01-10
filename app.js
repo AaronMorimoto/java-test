7cca823608004bebc1e176038787348bac8cb01b
AaronMorimoto <119596908+AaronMorimoto@users.noreply.github.com>
2023-01-10 13:54:20 +0000

    [java test6]

--- app.js
+++ app.js
@@ -36,3 +36,37 @@ if(orange < apple){
 } else{
   alert('みかんの値段がりんごより高い');
 }
+
+
+let max = 100;
+let num = 1;
+let count = 0;
+
+// 引数の条件がtrueの間、波括弧内部の処理が繰り返される。
+//while(num < max){
+  //num = num * 2;
+  //count = count + 1;
+//}
+
+//alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+
+do{
+  //最低1回は行われる処理
+  num = num * 100;
+}while(num < max){
+  num = num * 2;
+  count = count + 1;
+}
+
+alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+
+let i;
+let num1 = 0;
+
+for(i = 1; i < 11; i++){
+  num1 = num1 + i;
+}
+
+alert('1から10まで足し算した結果は' + num1 + 'です');
